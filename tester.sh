663e2484c26dc981448f830c6b654fe447679ee4
Administrator <gregoire@vodart.fr>
2022-04-12 11:07:58 +0200

    gregoire finish

--- tester.sh
+++ tester.sh
@@ -6,7 +6,7 @@
 #    By: gneve <marvin@42.fr>                       +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/04/12 10:02:32 by gneve             #+#    #+#              #
-#    Updated: 2022/04/12 10:36:11 by gneve            ###   ########.fr        #
+#    Updated: 2022/04/12 11:02:34 by gneve            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -21,9 +21,9 @@ i=0
 while True
 do 
 	head -c 10000000 /dev/random | base64 | head -c 10 >> testcase/encoded"$i".html
-	echo $(curl -s -o /dev/null -w '%{content_type}' $1 + "encoded$i.html") >> log/testn"$i".log
-	echo $(curl -s -o /dev/null -w "%{time_total}\n" -H "Pragma: no-cache" $1 + "encoded$i.html") >> log/loadtimen"$i".log
-	echo $(curl -s  $1 + "encoded$i.html") | wc -c >> log/contentln"$i".log
+	echo $(curl -s -o /dev/null -w '%{content_type}' "$1encoded$i.html") >> log/testn"$i".log
+	echo $(curl -s -o /dev/null -w "%{time_total}\n" -H "Pragma: no-cache" "$1encoded$i.html") >> log/loadtimen"$i".log
+	echo $(curl -s  "$1encoded$i.html") | wc -c >> log/contentln"$i".log
 #	echo $(curl -s -o /dev/null -w '%{content_type}' $site + encoded) >> log/test"$i".log
 #	echo $(curl -s -o /dev/null -w '%{time_total}\n' -H "Pragma: no-cache" $site) >> log/loadtime"$i".log
 #	echo $(curl -s $site) | wc -c >> log/contentl"$i".log
